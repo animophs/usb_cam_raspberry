f3895d2e139ae62f82051dd5e641521a65e088d2
ThanhNK <animorphs131@gmail.com>
2019-03-28 15:42:39 +0700

    the first commit, tested with rasp 3B+ (raspbian stretch) and PS3 eye

---
+++ README.md
@@ -0,0 +1,2 @@
+this sample is demo for Raspberry Pi 3B+ and Usb cam (PS3 eye)
+rasbian GNU/Linux 9.8 (stretch)
---
+++ usb_cam.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os
+import pygame, sys
+import time
+
+from pygame.locals import *
+import pygame.camera
+
+width = 640
+height = 480
+
+#initialise pygame   
+pygame.init()
+pygame.camera.init()
+cam = pygame.camera.Camera("/dev/video0",(width,height))
+cam.start()
+
+#setup window
+windowSurfaceObj = pygame.display.set_mode((width,height),1,16)
+pygame.display.set_caption('Camera')
+
+#take a picture
+try:
+	while True:
+		image = cam.get_image()
+		#display the picture
+		catSurfaceObj = image
+		windowSurfaceObj.blit(catSurfaceObj,(0,0))
+		pygame.display.update()
+		time.sleep(0.05)
+except KeyboardInterrupt:
+	print 'exit now'
+	cam.stop()
+	#save picture
+	pygame.image.save(windowSurfaceObj,'picture.jpg')
